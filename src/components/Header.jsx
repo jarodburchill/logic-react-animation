0b3cdd687b3b9cf24046c4c001f809588c207198
BURCHILL <jarodburchill@gmail.com>
2019-07-18 12:00:42 -0400

    first animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import Image from "./Image";
-import Logic from '../img/Logic.png'
+import Logic from '../img/Logic.png';
+import { Tween, Timeline } from 'react-gsap';
+import { Power2 } from 'gsap';
 
 const Header = () => {
   return (
-    <Image
-      src={Logic}
-      alt="Logic"
-    />
+    <Timeline
+      target={
+        <div>
+          <Image
+            src={Logic}
+            alt="Logic"
+          />
+        </div>
+      }
+    >
+      <Tween from={{ opacity: 0 }} to={{ opacity: 1, ease: Power2.easeInOut }} />
+      <Tween to={{ x: '50px' }} />
+    </Timeline>
   );
 }
 
