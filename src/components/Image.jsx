1757415c9fcc6ee44c9ac902099bfd760509c26e
BURCHILL <jarodburchill@gmail.com>
2019-07-18 14:52:46 -0400

    Code refactor

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,36 +3,26 @@ import { Tween, Timeline } from 'react-gsap';
 import { Power2 } from 'gsap';
 
 const Image = (props) => {
+  let startX;
   if (props.index % 2 === 0) {
-    return (
-      <Timeline
-        target={
-          <div>
-            <img src={props.src} alt={props.alt} className={props.alt} />
-          </div>
-        }
-        duration={1.5}
-      >
-        <Tween from={{ x: "-200%" }} to={{ x: "0%", ease: Power2.easeInOut }} />
-        <Tween from={{ y: "75%" }} to={{ y: "0%", ease: Power2.easeInOut }} />
-      </Timeline>
-    );
+    startX = "-200%";
   }
   else {
-    return (
-      <Timeline
-        target={
-          <div>
-            <img src={props.src} alt={props.alt} className={props.alt} />
-          </div>
-        }
-        duration={1.5}
-      >
-        <Tween from={{ x: "200%" }} to={{ x: "0%", ease: Power2.easeInOut }} />
-        <Tween from={{ y: "75%" }} to={{ y: "0%", ease: Power2.easeInOut }} />
-      </Timeline>
-    );
+    startX = "200%"
   }
+  return (
+    <Timeline
+      target={
+        <div>
+          <img src={props.src} alt={props.alt} className={props.alt} />
+        </div>
+      }
+      duration={1.5}
+    >
+      <Tween from={{ x: startX }} to={{ x: "0%", ease: Power2.easeInOut }} />
+      <Tween from={{ y: "75%" }} to={{ y: "0%", ease: Power2.easeInOut }} />
+    </Timeline>
+  );
 }
 
 export default Image;
